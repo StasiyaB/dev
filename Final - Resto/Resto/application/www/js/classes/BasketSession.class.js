1b6154605232bd77c6c1e7b4a69b549b385341fb
StasiyaB <baryash.anastasiya@gmail.com>
2019-01-31 17:09:54 +0100

    resto day 3

--- Final - Resto/Resto/application/www/js/classes/BasketSession.class.js
+++ Final - Resto/Resto/application/www/js/classes/BasketSession.class.js
@@ -1,1 +1,60 @@
 'use strict';
+
+var BasketSession = function() {
+
+  this.item = null;
+  this.load();
+
+}
+
+BasketSession.prototype.load = function (){
+
+  loadDataFromDomStorage('basket');
+
+  if (this.item == null) {
+    this.item = [];
+  }
+  this.build();
+}
+
+BasketSession.prototype.save = function () {
+
+  saveDataToDomStorage('basket', this.item);
+}
+
+BasketSession.prototype.add = function(mealId, name, quantity, salePrice) {
+
+  mealId    = parseInt(mealId);
+  quantity  = parseInt(quantity);
+  salePrice = parseFloat(salePrice);
+
+  for (var i = 0; i < this.item.length; i ++){
+
+    if (this.item[i].mealId == mealId){
+
+      this.item[i].quantity += quantity;
+
+      this.save();
+      this.build();
+
+      return;
+    }
+  }
+
+  this.item.push ({
+
+    mealId    : mealId,
+    name      : name,
+    quantity  : quantity,
+    salePrice : salePrice
+  });
+    //console.log(this.item);
+    this.save();
+    this.build();
+}
+
+BasketSession.prototype.build = function (response) {
+
+  console.log(response);
+
+}
--- Final - Resto/Resto/application/www/js/classes/OrderForm.class.js
+++ Final - Resto/Resto/application/www/js/classes/OrderForm.class.js
@@ -4,18 +4,22 @@
 
 var OrderForm = function() {
 
-  //this.basket = new BasketSession();
+  this.basket = new BasketSession();
 
   this.menu = document.getElementById('meal');
   this.onClickExecute();
   this.menu.addEventListener('change', this.onClickExecute.bind(this));
 
+  this.order = document.getElementById('order-summary');
+
+  this.validateButton = document.getElementById('validateMeal');
+  this.validateButton.addEventListener('click', this.mealToOrder.bind(this));
 }
 
 OrderForm.prototype.onClickExecute = function() {
   var id = this.menu.value;
-  console.log(id);
-  console.log(getRequestUrl() +'/meal?id='+ id);
+  //console.log(id);
+  //console.log(getRequestUrl() +'/meal?id='+ id);
   $.getJSON(getRequestUrl() +'/meal?id='+ id, this.changeMeal);
 
 }
@@ -27,5 +31,23 @@ OrderForm.prototype.changeMeal = function(response) {
   $("#meal-details").html('');
   $("#meal-details").append('<img src ="'+getWwwUrl()+ '/images/meals/' +response.Photo+'"/>');
   $("#meal-details").append('<p>'+response.Description+'</p>');
-  $("#meal-details").append('<span> Prix : '+'<strong>'+response.SalePrice+' € </strong>'+'</span>');
+  $("#meal-details").append('<span> Prix : '+'<strong>'+parseFloat(response.SalePrice).toFixed(2)+'</strong> €</span>');
+}
+
+OrderForm.prototype.mealToOrder = function(event) {
+
+  event.preventDefault();
+  //console.log('food');
+
+  var mealId = $('#meal').val();
+  var name = $('#meal').find('option:selected').text();
+  var quantity = $('#quantity').val();
+  var salePrice = $('#meal-details strong').text();
+  //console.log(mealId, name, quantity, salePrice);
+  //console.log(name);
+  //console.log(quantity);
+  //console.log(salePrice);
+
+  this.basket.add(mealId, name, quantity, salePrice);
+
 }
